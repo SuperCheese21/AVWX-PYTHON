bcbd31f98a8794c6f26557fca095384e375600c9
SuperCheese21 <ethan.shields21@gmail.com>
2019-08-09 01:00:17 -0700

    Added str method in airport model

--- airports/models.py
+++ airports/models.py
@@ -14,3 +14,6 @@ class Airport(models.Model):
     region = models.CharField(max_length=16)
     municipality = models.CharField(max_length=128)
     scheduled_service = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.icao
