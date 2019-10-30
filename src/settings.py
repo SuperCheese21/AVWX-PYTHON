084fd370cd4ec1dfbfa088743ea94cadf6f99658
SuperCheese21 <ethan.shields21@gmail.com>
2019-10-30 15:33:36 -0700

    Moved frontend directory to root directory

--- src/settings.py
+++ src/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "src.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "src/frontend/build")],
+        "DIRS": [os.path.join(BASE_DIR, "frontend/build")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -117,5 +117,5 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "src/frontend/build/static")
+    os.path.join(BASE_DIR, "frontend/build/static")
 ]
